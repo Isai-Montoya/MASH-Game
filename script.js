ef209b5e397ab11fa072261f64b2c1b07ca7a7ae
Isai-Montoya <112031467+Isai-Montoya@users.noreply.github.com>
2022-09-04 08:41:18 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,268 +1,74 @@
-      
-		
-		function start_mash() {
-		document.getElementsByTagName("button")[0].innerHTML = "Restart Game";
-			var player_name = prompt("Please enter your name");
-			if (player_name != null) {
-			document.getElementById("mash_title").innerHTML =
-			player_name + "'s MASH";}
-			document.getElementById("mash_house").style.display = "inline-block";
-			
-
-//player selection
-		//Marriage
-			
-			document.getElementById("marriagetext").style.display = "inline-block";
-			 love_choice1 = prompt("Pick a love interest")
-				if (love_choice1 != null) {
-					lc_list = document.getElementsByTagName("OL")[1];
-					lc_list.getElementsByTagName("LI")[0].innerHTML = love_choice1;}
-				
-				if (love_choice1 != null) {love_choice2 = prompt("Pick a second love interest")}
-				if (love_choice2 != null){
-					lc_list.getElementsByTagName("LI")[1].innerHTML = love_choice2;}
-
-				if (love_choice2 != null){love_choice3 = prompt("Pick a third love interest")}
-				if (love_choice3 != null) {
-					lc_list.getElementsByTagName("LI")[2].innerHTML = love_choice3;}
-									
-				if (love_choice3 != null) {love_choice4 = prompt ("Pick a fourth and final love interest")}
-				if (love_choice4 != null) {
-					lc_list.getElementsByTagName("LI")[3].innerHTML = love_choice4;};
-
-			
-				
-		
-//Occupation
-			if (love_choice4 != null) {document.getElementById("occupationtext").style.display = "inline-block";
-			 job_choice1 = prompt("Pick a job")};
-				if (job_choice1 != null) {
-					jc_list = document.getElementsByTagName("OL")[2];
-					jc_list.getElementsByTagName("LI")[0].innerHTML = job_choice1;}
-			 if (job_choice1 != null) {job_choice2 = prompt("Pick a second job")}
-				if (job_choice2 != null) {
-					jc_list.getElementsByTagName("LI")[1].innerHTML = job_choice2;}
-			 if (job_choice2 != null) {job_choice3 = prompt("Pick a third job")}
-				if (job_choice3 != null) {	
-					jc_list.getElementsByTagName("LI")[2].innerHTML = job_choice3;}
-			 if (job_choice3 != null) {job_choice4 = prompt("Pick a fourth and final job")}
-				if (job_choice4 != null) {
-					jc_list.getElementsByTagName("LI")[3].innerHTML = job_choice4;}
-
-
- 
-
-//Transportation
-			if (job_choice4 != null) {document.getElementById("transportationtext").style.display = "inline-block";
-			 car_choice1 = prompt("Pick a car/mode of transport")}
-				if (car_choice1 != null) {
-					cc_list = document.getElementsByTagName("OL")[3];
-					cc_list.getElementsByTagName("LI")[0].innerHTML = car_choice1;}
-			 if (car_choice1 != null) {car_choice2 = prompt("Pick a second car")}
-				if (car_choice2 != null) {
-					cc_list.getElementsByTagName("LI")[1].innerHTML = car_choice2;}
-			 if (car_choice2 != null) {car_choice3 = prompt("Pick a third car")}
-				if (car_choice3 != null) {
-					cc_list.getElementsByTagName("LI")[2].innerHTML = car_choice3;}
-			 if (car_choice3 != null) {car_choice4 = prompt("Pick a fourth and final car")}
-				if (car_choice4 != null) {
-					cc_list.getElementsByTagName("LI")[3].innerHTML = car_choice4;}
-
-
-
-        
-//Money
-			if (car_choice4 != null) {document.getElementById("moneytext").style.display = "inline-block";
-			 money_choice1 = prompt("Pick a yearly salary")}
-				if (money_choice1 != null) {
-					mc_list = document.getElementsByTagName("OL")[4];
-					mc_list.getElementsByTagName("LI")[0].innerHTML = money_choice1;}
-			 if (money_choice1 != null) {money_choice2 = prompt("Pick another yearly salary")}
-				if (money_choice2 != null) {
-					mc_list.getElementsByTagName("LI")[1].innerHTML = money_choice2;}
-			 if (money_choice2 != null) {money_choice3 = prompt("Pick a third yearly salary")}
-				if (money_choice3 != null) {
-					mc_list.getElementsByTagName("LI")[2].innerHTML = money_choice3;}
-			if (money_choice3 != null) {money_choice4 = prompt("Pick a fourth and final salary")}
-				if (money_choice4 != null) {
-					mc_list.getElementsByTagName("LI")[3].innerHTML = money_choice4;}
-
- 
-		
-//Pets
-			if (money_choice4 != null) {document.getElementById("petstext").style.display = "inline-block";
-			 pet_choice1 = prompt("Pick an animal")}
-				if (pet_choice1 != null) {
-					pc_list = document.getElementsByTagName("OL")[5];
-					pc_list.getElementsByTagName("LI")[0].innerHTML = pet_choice1;}
-			 if (pet_choice1 != null) {pet_choice2 = prompt("Pick another animal")}
-				if (pet_choice2 != null) {
-					pc_list.getElementsByTagName("LI")[1].innerHTML = pet_choice2;}
-			 if (pet_choice2 != null) {pet_choice3 = prompt("Pick a third animal")}
-				if (pet_choice3 != null) {
-					pc_list.getElementsByTagName("LI")[2].innerHTML = pet_choice3;}
-			 if (pet_choice3 != null) {pet_choice4 = prompt("Pick a fourth and final animal")}
-				if (pet_choice4 != null) {
-					pc_list.getElementsByTagName("LI")[3].innerHTML = pet_choice4;}
-
- 
-        
-//Places
-			if (pet_choice4 != null) {document.getElementById("placestext").style.display = "inline-block";
-			 place_choice1 = prompt("Pick somewhere to live")}
-				if (place_choice1 != null) {
-					plc_list = document.getElementsByTagName("OL")[6];
-					plc_list.getElementsByTagName("LI")[0].innerHTML = place_choice1;}
-			 if (place_choice1 != null) {place_choice2 = prompt("Pick another place")}
-				if (place_choice2 != null) {
-					plc_list.getElementsByTagName("LI")[1].innerHTML = place_choice2;}
-			 if (place_choice2 != null) {place_choice3 = prompt("Pick a third place")}
-				if (place_choice3 != null) {
-					plc_list.getElementsByTagName("LI")[2].innerHTML = place_choice3;}
-			 if (place_choice3 != null) {place_choice4 = prompt("Pick a fourth and final place")}
-				if (place_choice4 != null) {
-					plc_list.getElementsByTagName("LI")[3].innerHTML = place_choice4;}
-
-
-	 
-//result calculation    
-				
-		document.getElementById("futurebutton").style.display = "block";
-}
-//mash result
-		function mash_results(){
-		
-  //clear background
-  var xx = document.querySelectorAll("LI");
-
-for (var i = 0; i < xx.length; i++) {
-    xx[i].style.backgroundColor="transparent";
+    // New function called random_number that takes one parameter, num (or a number)
+function random_number(num) {
+	// Get a random number between 0 and a passed-in number
+	var num = num || 4  // If no number passed in, default to 4
+	return Math.floor(Math.random() * num); // Round the answer down (floor) of a random number between 0 and 1 and multiply it by a number. Then return a value and exit the function.
 }
 
-		mashHouse = ["Mansion", "House", "Apartment", "Shack"];
-		
-
-function number_to_love(number){
-     if (number == 1)
-         {return love_choice1}
-     else if (number == 2){
-         return love_choice2}
-     else if (number == 3){
-         return love_choice3}
-     else if (number == 4){
-         return love_choice4}
-     else{
-         alert("That number is not in the correct range")}}				
-			alert("Your future is...(imagine a drumroll or tense gameshow music then press ok)");
-			mash_number = Math.floor((Math.random() * 4));
-			mashHouseResult = mashHouse [mash_number];
-			alert("You live in a " + mashHouseResult);
-			mash_list = document.getElementsByTagName("OL")[0];
-			mash_list.getElementsByTagName("LI")[mash_number].style.backgroundColor = "yellow";
-			
-			
-			
-			//love result
-var love_number = Math.floor((Math.random() * 4) + 1);
-alert ("You are married to " + number_to_love(love_number));
-lc_list.getElementsByTagName("LI")[love_number - 1].style.backgroundColor = "yellow";
-
-
-function number_to_job(number){
-     if (number == 1)
-         {return job_choice1}
-     else if (number == 2)
-         {return job_choice2}
-     else if (number == 3)
-         {return job_choice3}
-     else if (number == 4)
-         {return job_choice4}
-     else{
-         alert ("That number is not in the correct range")}}
-//job result
-var job_number = Math.floor((Math.random() * 4) + 1);
-alert ("You work as a " + number_to_job(job_number));
-jc_list.getElementsByTagName("LI")[job_number - 1].style.backgroundColor = "yellow";
-
-function number_to_car(number){
-    if (number == 1)
-        {return car_choice1}
-    else if (number == 2)
-        {return car_choice2}
-    else if (number == 3)
-        {return car_choice3}
-    else if (number == 4)
-        {return car_choice4}
-    else{
-        alert("That number is not in the correct range")}}
-//car result
-var car_number = Math.floor((Math.random() * 4) + 1);
-alert ("You drive a " + number_to_car(car_number))
-cc_list.getElementsByTagName("LI")[car_number - 1].style.backgroundColor = "yellow";
-
-function number_to_money(number){
-    if (number == 1)
-        {return money_choice1}
-    else if (number == 2)
-        {return money_choice2}
-    else if (number == 3)
-        {return money_choice3}
-    else if (number == 4)
-        {return money_choice4}
-    else{
-        alert ("That number is not in the correct range")}}  
-//money result
-var money_number = Math.floor((Math.random() * 4) + 1);
-alert ("You make " + number_to_money(money_number) + " a year")
-mc_list.getElementsByTagName("LI")[money_number - 1].style.backgroundColor = "yellow";
-
-function number_to_pet(number){
-     if (number == 1)
-         {return pet_choice1}
-     else if (number == 2)
-         {return pet_choice2}
-     else if (number == 3)
-         {return pet_choice3}
-     else if (number == 4)
-         {return pet_choice4}
-     else{
-         alert ("That number is not in the correct range")} 
-		 }
-//pet result
-var pet_number = Math.floor((Math.random() * 4) + 1);
-alert ("You have a pet "+ number_to_pet(pet_number))
-pc_list.getElementsByTagName("LI")[pet_number - 1].style.backgroundColor = "yellow";
-
- function number_to_place(number){
-     if (number == 1){
-         return place_choice1}
-     else if (number == 2)
-         {return place_choice2}
-     else if (number == 3)
-         {return place_choice3}
-     else if (number == 4)
-         {return place_choice4}
-     else{
-         alert ("That number is not in the correct range")} 
-}	
-
-//place result
-var place_number = Math.floor((Math.random() * 4) + 1);
-alert ("and You live in " + number_to_place(place_number))	
-plc_list.getElementsByTagName("LI")[place_number - 1].style.backgroundColor = "yellow";	
-document.getElementsByTagName("button")[2].innerHTML = "Re-Calculate Future";
+// New function called mash_choice that doesn't take any parameters 
+function mash_choice() {
+	// Since MASH is a special case, give it its own list
+	var mash = ['mansion', 'apartment', 'shack', 'house'];  // The array of choices to pick from 
+	var randomNum = random_number(4);  // Use the above function to get a number between 0 and 4
+	return mash[randomNum];  // Return the list item the random number function just picked and exit the function 
 }
-function change_background(){
 
- var pageimage = document.getElementsByTagName("body")[0];
-  
-    if (pageimage.style.backgroundImage.match("https://media.giphy.com/media/10FNGnuUcdEeUE/giphy.gif")) {
-        pageimage.style.backgroundImage = "url(http://www.fumcs.com/wp-content/uploads/2012/12/notebook-paper-1-lines.jpg )";
-		console
-		
-    } 
-	else {
-        pageimage.style.backgroundImage = "url(https://media.giphy.com/media/10FNGnuUcdEeUE/giphy.gif )";
-		
-    }
+// Get a random answer from the available answers in a given category
+function get_answer(category) { 
+	var choices = [];  // A blank array to hold the user provided answer  
+	var selector = 'input[name="' + category + '[]"]';  // Build a CSS selector for the blanks in our passed in category 
+	var inputs = document.querySelectorAll(selector);  // Get all of the inputs that match our selector 
+	var answer;
+
+	for (var i = 0; i < inputs.length; i++) {  // Begin a for loop that will run through the code. i++ = add one to the counter which is "i"
+		answer = inputs[i].value;  // Get the input with the index value of the counter and get the value ie. if they typed in dog, you get back "dog" 
+		if (answer !== '') {  // If answer doesn't equal a blank... !== means doesn't equal 
+			choices.push(answer); //...add it to the end of the list 
+		}
 	}
+	return choices[random_number(choices.length)];   // Pick and return a random choice choice.length = number of answers the user provided in that category 
+}
+
+// Function to find the spans that need to be filled
+function fill_in_answers(answers) {
+	var home = document.querySelector('#home');  // This says make a new variable and find the HTML tag that has the ID of "home" 
+	var answer_2 = document.querySelector('#answer_2');
+	var answer_3 = document.querySelector('#answer_3');
+	var answer_1 = document.querySelector('#answer_1');
+	
+	// Fill them with the provided answers
+	home.innerText = answers['mash'];
+	answer_2.innerText = answers['answer_2'];
+	answer_3.innerText = answers['answer_3'];
+	answer_1.innerText = answers['answer_1'];
+	home.innerHTML = answers.mash;  // Change the content of the element in the HTML doc with the id "home" to the "mash" value in answers 
+	answer_2.innerHTML = answers.answer_2;  // Change the content of the element in the HTML doc with the id "career" to the "career" value in answers 
+	answer_3.innerHTML = answers.answer_3;
+	answer_1.innerHTML = answers.answer_1;
+}
+
+// Button submit handler function
+function handle_submission(evt) {
+	evt.preventDefault();  // Stop the form from reloading the page 
+	evt.stopPropagation();  // Stop the form from reloading the page
+	
+	// Build up our answers object
+	var answers = {
+	    'mash': mash_choice(),
+	    'answer_2': get_answer('answer_2'),
+	    'answer_3': get_answer('answer_3'),
+	    'answer_1': get_answer('answer_1')
+	}
+	// Fill in the answers
+	fill_in_answers(answers);
+	
+	var answer_div = document.querySelector('#answers');
+	answer_div.classList.add('show');
+}
+
+// Find the form on the page and attach a handler for when it's submitted
+var form = document.querySelector('#mash');
+
+// Anytime the form is submitted, we want to call the function handle_submission 
+form.addEventListener('submit', handle_submission);  
+	
